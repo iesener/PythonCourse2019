42309f904fa6f10f49c301aa470d83e3dcff440b
Ipek <ipekecesener@gmail.com>
2019-08-11 17:34:44 -0500

    Minor fixes on homework 1
    Minor fixes

--- homeworks/hw1/Homework01_Ipek.py
+++ homeworks/hw1/Homework01_Ipek.py
@@ -11,18 +11,26 @@ class Portfolio():
 		self.time = []
 	#Prints the Portfolio
 	def __str__(self):
-		return "cash: %s \nstock: %s \nMutual Funds: %s" %(self.CashAmount, self.StockShare, self.MF)
+		ss = []
+		mf = []
+		for i in list(self.StockShare.keys()):
+			x = "%s %s"%(self.StockShare[i][0], i)
+			ss.append(x)
+		for i in list(self.MF.keys()):
+			y = "%.2f %s"%(self.MF[i], i)
+			mf.append(y)
+		return "cash: %.2f \nstock: %s \nmutual funds: %s" %(self.CashAmount, ss, mf)
 	def addCash(self, amount):
 		self.CashAmount += amount
-		self.hist.append("+ $%s" %amount)
-		self.balance.append(self.CashAmount)
+		self.hist.append("+ $%.2f" %amount)
+		self.balance.append("$%.2f" %self.CashAmount)
 		now = datetime.now()
 		self.time.append(now.strftime("%d-%b-%Y %H:%M:%S"))
 	def withdrawCash(self,amount):
 		if amount <= self.CashAmount:
 			self.CashAmount -= amount
-			self.hist.append("- $%s" %amount)
-			self.balance.append(self.CashAmount)
+			self.hist.append("- $%.2f" %amount)
+			self.balance.append("$%.2f" %self.CashAmount)
 			now = datetime.now()
 			self.time.append(now.strftime("%d-%b-%Y %H:%M:%S"))
 		else:
@@ -37,20 +45,19 @@ class Portfolio():
 				self.StockShare.update(newstockportfolio)
 			self.CashAmount -= stock.price * share
 			self.hist.append("+ %s %s" % (share, stock.symbol))
-			self.balance.append(self.CashAmount)
+			self.balance.append("$%.2f" %self.CashAmount)
 			now = datetime.now()
 			self.time.append(now.strftime("%d-%b-%Y %H:%M:%S"))
 		else:
 			print("Non-sufficient Funds")
 	def sellStock(self, symbol, share):
 		x=sp.random.uniform(low=(self.StockShare[symbol][1] * (1/2)) ,high=(self.StockShare[symbol][1] * (3/2)),size=1)
-		uni = round(x[0],2)
 		if symbol in list(self.StockShare.keys()):
 			if share <= self.StockShare[symbol][0]:
 				self.StockShare.update([(symbol, [(self.StockShare[symbol][0] - share),self.StockShare[symbol][1]])])
-				self.CashAmount += uni * share
+				self.CashAmount += x[0] * share
 				self.hist.append("- %s %s" % (share, symbol))
-				self.balance.append(self.CashAmount)
+				self.balance.append("$%.2f" %self.CashAmount)
 				now = datetime.now()
 				self.time.append(now.strftime("%d-%b-%Y %H:%M:%S"))
 			else:
@@ -66,21 +73,35 @@ class Portfolio():
 			else:
 				self.MF.update(newMF)
 			self.CashAmount -= share
+			self.hist.append("+ %.2f %s" % (share, funds.symbol))
+			self.balance.append("$%.2f" %self.CashAmount)
+			now = datetime.now()
+			self.time.append(now.strftime("%d-%b-%Y %H:%M:%S"))
 		else:
 			print("Non-sufficient Funds")
-	def history(self):
-		print('Transaction \t\tBalance \tDate')
-		for i,j,k in zip(self.hist,self.balance,self.time):
-			print(i, '{:>20}'.format('%s' j), '{:>20}'.format(''), k)
-
-
-	def sellMutualFund():
+	def sellMutualFund(self, symbol, share):
 		x=sp.random.uniform(0.9,1.2,1)
-		uni = round(x[0],2)		
+		if symbol in list(self.MF.keys()):
+			if share <= self.MF[symbol]:
+				self.MF.update([(symbol, (self.MF[symbol] - share))])
+				self.CashAmount -= x[0] * share
+				self.hist.append("- %s %s" % (share, symbol))
+				self.balance.append("$%.2f" %self.CashAmount)
+				now = datetime.now()
+				self.time.append(now.strftime("%d-%b-%Y %H:%M:%S"))
+			else:
+				print("Non-sufficient Shares")
+		else:
+			print("Non-Existing Mutual Funds")
+	def history(self):
+		data = list(map(list, zip(*[self.hist,self.balance,self.time])))
+		dash = '-' * 55
+		print(dash)
+		print('{:<15s}{:^14s}{:^26s}'.format("Transaction", "Balance", "Date"))
+		print(dash)
+		for i in range(len(data)):
+			print('{:<10s}{:>15s}{:>30s}'.format(data[i][0],data[i][1],data[i][2]))
 
-
-for n,g in zip(x,y):
-	print(n + '\t\t\t' + g)
 
 
 class Stock:
@@ -92,31 +113,4 @@ class Stock:
 class MutualFund:
 	def __init__(self, symbol):
 		self.symbol = symbol
-
-
-port = Portfolio()
-port.addCash(500)
-port.addCash(100)
-port.withdrawCash(40)
-port.withdrawCash(35)
-
-deneme = Stock(5, "HTM")
-deneme2 = Stock(10, "BLB")
-port.buyStock(5,deneme)
-port.buyStock(8,deneme2)
-print(port)
-port.buyStock(2,deneme2)
-print(port)
-port.sellStock("HTM",2)
-print(port)
-port.sellStock("BLB",3)
-print(port)
-
-
-mf1 = MutualFund("BRT")
-mf2 = MutualFund("GHT")
-port.buyMutualFund(10.3, mf1) 
-port.buyMutualFund(2, mf2)
-print(port)
-
 
